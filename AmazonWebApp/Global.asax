ff7689a79522a3b5b99428940a752beb1b796c9d
Taavi's PC <Taavi's PC@TAAVIPC>
2016-06-12 12:13:38 +0300

    Add project files.

---
+++ AmazonWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AmazonWebApp.MvcApplication" Language="C#" %>
---
+++ AmazonWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AmazonWebApp.Startup))]
+namespace AmazonWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
